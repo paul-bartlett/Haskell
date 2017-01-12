d3968ff5bdcbbf542840a15ea017b6c70f36384f
Paul Bartlett <A6L2HZZ@usac.mmm.com>
2017-01-12 17:42:13 -0500

    feels wrong

--- ValidatingCCNumbers.hs
+++ ValidatingCCNumbers.hs
@@ -7,7 +7,28 @@ main = do
 -- Convert positive Integers to a list of digits
 toDigits :: Integer -> [Integer]
 toDigits n
-	| n <= 0 	= []
-	| otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
+    | n <= 0    = []
+    | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
 
-toDigitsRev	:: Integer -> [Integer]
+-- Reverse a list of digits
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse $ toDigits n
+
+-- Doubles every other number
+doubleOtherNum :: [Integer] -> [Integer]
+doubleOtherNum []       = []
+doubleOtherNum (x:[])   = [x]
+doubleOtherNum (x:y:zs) = x : (y * 2) : doubleOtherNum zs
+
+-- Reverses a list to double each other number starting from the end, then reverses it back to the original order
+doubleEveryOther :: Integer -> [Integer]
+doubleEveryOther n = reverse $ doubleOtherNum $ toDigitsRev n
+
+-- Sums a list of digits
+sumNums :: [Integer] -> Integer
+sumNums []     = 0
+sumNums (x:xs) = x + (sumNums xs)
+
+-- Sums a list of doubled digits
+sumDigits :: Integer -> Integer
+sumDigits n = sumNums $ doubleEveryOther n
