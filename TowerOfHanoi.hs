2b5034e1c893d635a4d1dc5525a7cb9705491250
paul-bartlett <bartlett_paul@hotmail.com>
2017-01-17 11:33:29 -0500

    Towers of hanoi done

--- TowerOfHanoi.hs
+++ TowerOfHanoi.hs
@@ -1,3 +1,5 @@
 type Peg = String
 type Move = (Peg, Peg)
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 a b c = [(a, b)]
+hanoi n a b c = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
