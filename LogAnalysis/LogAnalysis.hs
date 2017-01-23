a21cf4d435aedb24cfb04754ed817aeaaf4eeb2c
paul-bartlett <bartlett_paul@hotmail.com>
2017-01-23 16:49:24 -0500

    brain think

--- LogAnalysis/LogAnalysis.hs
+++ LogAnalysis/LogAnalysis.hs
@@ -3,6 +3,9 @@ module LogAnalysis where
 
 import Log
 
+stringSplit :: String -> [String]
+stringSplit s = words s
+
 -- Match message type from letter
 messageType :: String -> MessageType
 messageType x = case x of
@@ -13,5 +16,3 @@ messageType x = case x of
 -- Parse message
 parseMessage :: String -> LogMessage
 parseMessage = LogMessage . take 2 . lines
-
-
