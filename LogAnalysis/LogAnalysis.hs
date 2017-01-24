7a833b67ac592f9e627dd9f9d4f011c2cfdd9c22
paul-bartlett <bartlett_paul@hotmail.com>
2017-01-24 10:56:32 -0500

    gross workaround, but log parcing works for individual messages

--- LogAnalysis/LogAnalysis.hs
+++ LogAnalysis/LogAnalysis.hs
@@ -6,13 +6,14 @@ import Log
 -- Match message type from letter
 messageType :: String -> [String] -> LogMessage
 messageType x s = case x of
-                "I" -> LogMessage Info (stampParse s) (unwords $ tail s)
-                "W" -> LogMessage Warning (stampParse s) (unwords $ tail s)
-                --'E' -> LogMessage Error (read head s) stampParse $ tail s
+                "I" -> LogMessage Info (intParse s) (unwords $ tail s)
+                "W" -> LogMessage Warning (intParse s) (unwords $ tail s)
+                "E" -> LogMessage (Error (intParse s)) (intParse $ tail s) (unwords $ drop 2 s)
+                _   -> Unknown $ unwords (x:s)
 
--- Parse the stamp number
-stampParse :: [String] -> TimeStamp
-stampParse s = read $ head s
+-- Parse int for Error ID and the stamp number
+intParse :: [String] -> Int
+intParse s = read $ head s
 
 -- Parse message
 parseMessage :: String -> LogMessage
