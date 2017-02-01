ce6a25767b5734de3c08ec92b0be7c2d33dd9a6e
paul-bartlett <bartlett_paul@hotmail.com>
2017-02-01 11:37:59 -0500

    cheated / fixed to work with multiple logs

--- LogAnalysis/LogAnalysis.hs
+++ LogAnalysis/LogAnalysis.hs
@@ -3,21 +3,13 @@ module LogAnalysis where
 
 import Log
 
--- Parse int for Error ID and the stamp number
-intParse :: [String] -> Int
-intParse s = read $ head s
-
--- Match message type from letter and parses into a log message
-messageType :: String -> [String] -> LogMessage
-messageType x s = case x of
-                "I" -> LogMessage Info (intParse s) (unwords $ tail s)
-                "W" -> LogMessage Warning (intParse s) (unwords $ tail s)
-                "E" -> LogMessage (Error (intParse s)) (intParse $ tail s) (unwords $ drop 2 s)
-                _   -> Unknown $ unwords (x:s)
-
--- Sends message type and remainder of message to be parsed
+-- Parses individual message into a log message
 parseMessage :: String -> LogMessage
-parseMessage s = messageType (head $ words s) (tail $ words s)
+parseMessage s = case words s of
+    ("I":n:msg)     -> LogMessage Info (read n) (unwords $ msg)
+    ("W":n:msg)     -> LogMessage Warning (read n) (unwords $ msg)
+    ("E":err:n:msg) -> LogMessage (Error $ read err) (read n) (unwords $ msg)
+    _               -> Unknown $ s
 
 parse :: String -> [LogMessage]
-parse s  = [parseMessage s]
+parse s  = map parseMessage $ lines s
