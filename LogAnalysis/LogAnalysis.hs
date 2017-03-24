42dc87bfcb5d8e12597967793e8b6b6da425a1b3
paul-bartlett <bartlett_paul@hotmail.com>
2017-03-24 12:34:20 -0400

    It's probably good

--- LogAnalysis/LogAnalysis.hs
+++ LogAnalysis/LogAnalysis.hs
@@ -18,7 +18,7 @@ parse s  = map parseMessage $ lines s
 -- Inserts a new LogMessage into an existing sorted MessageTree
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
-insert log@(LogMessage _ _ _) Leaf = Node Leaf log Leaf
+insert lg@(LogMessage _ _ _) Leaf = Node Leaf lg Leaf
 insert log1@(LogMessage _ time1 _) (Node l log2@(LogMessage _ time2 _) r)
     | time1 > time2 = Node l log2 (insert log1 r)
     | otherwise     = Node (insert log1 l) log2 r
@@ -32,7 +32,19 @@ build (logmessage:logmessages) = insert logmessage (build logmessages)
 -- Takes a sorted MessageTree and produces a sorted list of LogMessages from smallest to largest timestamp
 inOrder :: MessageTree -> [LogMessage]
 inOrder Leaf = []
-inOrder (Node l log r) = inOrder l ++ [log] ++ inOrder r
+inOrder (Node l lmsg r) = inOrder l ++ [lmsg] ++ inOrder r
+
+-- Filters LogMessages with defined severity
+logFilter :: Int -> LogMessage -> [String]
+logFilter severity (LogMessage (Error err) _ message)
+    | err > severity = [message]
+    | otherwise      = []
+logFilter _ _ = []
+
+logFilters :: [LogMessage] -> [String]
+logFilters [] = []
+logFilters (lmsg:lmsgs) = logFilter 50 lmsg ++ logFilters lmsgs
 
 -- Takes an unsorted list of LogMessages and returns a sorted list of messages with severity >50
 whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logmessages = logFilters $ inOrder $ build logmessages
