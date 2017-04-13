e2619c2048ba6540958a3c17f5ce9033a6f21078
paul-bartlett <bartlett_paul@hotmail.com>
2017-04-13 14:13:28 -0400

    local maxima done

---
+++ Local maxima/localMaxima.hs
@@ -0,0 +1,8 @@
+-- Finds all local maxima in a list of integers
+localMaxima :: [Integer] -> [Integer]
+localMaxima []       = []
+localMaxima (x:[])   = []
+localMaxima (x:y:[]) = []
+localMaxima (x:y:z:zs)
+    | y > x && y > z = y : localMaxima (y:z:zs)
+    | otherwise      = localMaxima (y:z:zs)
