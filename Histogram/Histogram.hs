05f5eed2fd2f159a9132ac83fdde408b529bf344
paul-bartlett <bartlett_paul@hotmail.com>
2017-05-08 10:00:49 -0400

    It counts stuff now

--- Histogram/Histogram.hs
+++ Histogram/Histogram.hs
@@ -1,3 +1,12 @@
 -- Takes an list of Integers between 0 and 9 and outputs a vertical histogram
-histogram :: [Integer] -> String
+--histogram :: [Integer] -> String
 -- do something with map +1 to an array of 0's
+--histogram lst = map [maximum lst..1]
+--Counts the number of occurences in a list
+count :: Eq a => a -> [a] -> Int
+count x = length . filter (==x)
+
+--Creates array of number of occurences in an Int list
+--ex: [1,3,4,1] -> [2,0,1,1]
+countMult :: [Int] -> [Int]
+countMult lst = map (`count` lst) [1..maximum lst]
