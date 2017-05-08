9ea286e5f3fc21d987e44531a2ccc4c26246e14f
paul-bartlett <bartlett_paul@hotmail.com>
2017-05-08 14:22:13 -0400

    Just need more of them newlines

--- Histogram/Histogram.hs
+++ Histogram/Histogram.hs
@@ -1,12 +1,20 @@
--- Takes an list of Integers between 0 and 9 and outputs a vertical histogram
---histogram :: [Integer] -> String
--- do something with map +1 to an array of 0's
---histogram lst = map [maximum lst..1]
+{-# OPTIONS_GHC -Wall #-}
 --Counts the number of occurences in a list
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (==x)
 
 --Creates array of number of occurences in an Int list
---ex: [1,3,4,1] -> [2,0,1,1]
+--ex: [1,3,4,1,9] -> [0,2,0,1,1,0,0,0,0,1]
 countMult :: [Int] -> [Int]
-countMult lst = map (`count` lst) [1..maximum lst]
+countMult lst = map (`count` lst) [1..9]
+
+--Creates the string for outputting
+outputSymbol :: Integer -> [Integer] -> String
+outputSymbol num (x:xs)
+    | x < num    = " " ++ outputSymbol num xs
+    | x >= num   = "*" ++ outputSymbol num xs
+outputSymbol _ _ = []
+
+--Takes an list of Integers between 0 and 9 and outputs a vertical histogram
+histogram :: [Integer] -> String
+histogram lst = foldr (++) [] $ map (`outputSymbol` lst) [1..9]
