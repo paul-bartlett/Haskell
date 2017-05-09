1b7edfe486c12011c4c71273cdbaf98af9dd896a
paul-bartlett <bartlett_paul@hotmail.com>
2017-05-09 09:09:15 -0400

    Type conversion kind of good but not really

--- Histogram/Histogram.hs
+++ Histogram/Histogram.hs
@@ -3,10 +3,10 @@
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (==x)
 
---Creates array of number of occurences in an Int list
+--Creates array of number of occurences in an Integer list
 --ex: [1,3,4,1,9] -> [0,2,0,1,1,0,0,0,0,1]
-countMult :: [Int] -> [Int]
-countMult lst = map (`count` lst) [1..9]
+countMult :: [Int] -> [Integer]
+countMult lst = map (toInteger) $ map (`count` lst) [1..9]
 
 --Creates the string for outputting
 outputSymbol :: Integer -> [Integer] -> String
@@ -19,5 +19,5 @@ histoPlot :: [Integer] -> String
 histoPlot lst = foldr (++) [] $ map (`outputSymbol` lst) [9,8..1]
 
 --Takes an list of Integers between 0 and 9 and outputs a vertical histogram
---histogram :: [Integer] -> String
---histogram lst = (histoPlot $ countMult lst) ++ "0123456789"
+histogram :: [Integer] -> String
+histogram lst = (histoPlot $ countMult lst) ++ "==========\n0123456789"
