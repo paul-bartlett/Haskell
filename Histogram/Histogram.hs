86966e543aa4d937a43c07211968d0ac98a4f10c
paul-bartlett <bartlett_paul@hotmail.com>
2017-05-09 09:41:47 -0400

    Everything is okay now

--- Histogram/Histogram.hs
+++ Histogram/Histogram.hs
@@ -5,8 +5,8 @@ count x = length . filter (==x)
 
 --Creates array of number of occurences in an Integer list
 --ex: [1,3,4,1,9] -> [0,2,0,1,1,0,0,0,0,1]
-countMult :: [Int] -> [Integer]
-countMult lst = map (toInteger) $ map (`count` lst) [1..9]
+countMult :: [Integer] -> [Integer]
+countMult lst = map (toInteger) $ map (`count` lst) [0..9]
 
 --Creates the string for outputting
 outputSymbol :: Integer -> [Integer] -> String
@@ -16,8 +16,8 @@ outputSymbol num (x:xs)
 outputSymbol _ _ = "\n"
 
 histoPlot :: [Integer] -> String
-histoPlot lst = foldr (++) [] $ map (`outputSymbol` lst) [9,8..1]
+histoPlot lst = foldr (++) [] $ map (`outputSymbol` lst) [maximum lst, maximum lst - 1..1]
 
 --Takes an list of Integers between 0 and 9 and outputs a vertical histogram
 histogram :: [Integer] -> String
-histogram lst = (histoPlot $ countMult lst) ++ "==========\n0123456789"
+histogram lst = (histoPlot $ countMult lst) ++ "==========\n0123456789\n"
