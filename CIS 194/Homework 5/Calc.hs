860b719c2f72b103a3baf60f9fa451eb404402f1
paul-bartlett <pbartle7@uwo.ca>
2018-06-01 15:53:58 -0400

    Exercise 2 complete

--- CIS 194/Homework 5/Calc.hs
+++ CIS 194/Homework 5/Calc.hs
@@ -2,10 +2,19 @@
 module Calc where
 
 import ExprT
+import Parser
 
 -- Exercise 1
 -- Adds and multiplies basic expressions of type ExprT
 eval :: ExprT -> Integer
-eval (Lit val) = val
+eval (Lit val)       = val
 eval (Add exp1 exp2) = (eval exp1) + (eval exp2)
 eval (Mul exp1 exp2) = (eval exp1) * (eval exp2)
+
+-- Exercise 2
+-- Parses expression and evaluates if valid
+evalStr :: String -> Maybe Integer
+evalStr s = case p of
+          Just val -> Just (eval val)
+          Nothing  -> Nothing
+          where p = parseExp Lit Add Mul s
