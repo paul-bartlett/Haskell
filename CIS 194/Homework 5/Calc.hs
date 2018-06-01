b793b53cb3b543ea13a40697fb51d1e7b02bcd0d
paul-bartlett <pbartle7@uwo.ca>
2018-06-01 13:42:29 -0400

    Exercise 1 complete

--- CIS 194/Homework 5/Calc.hs
+++ CIS 194/Homework 5/Calc.hs
@@ -3,4 +3,9 @@ module Calc where
 
 import ExprT
 
+-- Exercise 1
+-- Adds and multiplies basic expressions of type ExprT
 eval :: ExprT -> Integer
+eval (Lit val) = val
+eval (Add exp1 exp2) = (eval exp1) + (eval exp2)
+eval (Mul exp1 exp2) = (eval exp1) * (eval exp2)
