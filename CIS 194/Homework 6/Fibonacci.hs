9c04461284aac13fb8d5d67ce9f2cd30f49f9102
paul-bartlett <pbartle7@uwo.ca>
2018-06-02 15:22:16 -0400

    Exercises 1 and 2 are done

--- CIS 194/Homework 6/Fibonacci.hs
+++ CIS 194/Homework 6/Fibonacci.hs
@@ -1,6 +1,14 @@
 -- Exercise 1
 -- A recursive function to determine the nth Fibonacci number
 fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
 
 -- An infinite list of all Fibonacci numbers
 fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+-- Exercise 2
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
