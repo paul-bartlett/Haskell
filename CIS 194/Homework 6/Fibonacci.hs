81f541349dbd655c6bfc16802eefaaecb9c2bdff
paul-bartlett <pbartle7@uwo.ca>
2018-06-02 21:05:46 -0400

    Exercise 3 complete

--- CIS 194/Homework 6/Fibonacci.hs
+++ CIS 194/Homework 6/Fibonacci.hs
@@ -1,3 +1,7 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Fibonacci where
+
 -- Exercise 1
 -- A recursive function to determine the nth Fibonacci number
 fib :: Integer -> Integer
@@ -12,3 +16,12 @@ fibs1 = map fib [0..]
 -- Exercise 2
 fibs2 :: [Integer]
 fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
+
+-- Exercise 3
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+    show = show . take 20 . streamToList
