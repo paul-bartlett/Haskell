b7a2931a3fa6b339919dd96838141b5f842bd057
paul-bartlett <pbartle7@uwo.ca>
2018-06-11 20:35:09 -0400

    Done exercise 1

--- CIS 194/Homework 8/Employee.hs
+++ CIS 194/Homework 8/Employee.hs
@@ -1,6 +1,6 @@
 module Employee where
 
-import           Data.Tree
+import Data.Tree
 
 -- Employee names are represented by Strings.
 type Name = String
--- CIS 194/Homework 8/Party.hs
+++ CIS 194/Homework 8/Party.hs
@@ -1,5 +1,21 @@
-{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wall -fno-warn-orphans #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Party where
 
+import Employee
+
+-- Adds an Employee to the GuestList
 glCons :: Employee -> GuestList -> GuestList
+glCons emp (GL emps f) = GL (emp : emps) (f + empFun emp)
+
+-- Monoid instance for empty GuestList and appending 2 GuestLists
+instance Monoid GuestList where
+    mempty  = GL [] 0
+    mappend (GL emps1 f1) (GL emps2 f2) = GL (emps1 ++ emps2) (f1 + f2)
+
+-- Takes two GuestLists and return the one that is more fun
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1 gl2 
+    | gl1 > gl2 = gl1
+    | otherwise = gl2
